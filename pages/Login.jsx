445521bdfbd52f94cc12db4ae10a6d8ce749f68d
ReaperScythe13 <breonono13853@gmail.com>
2025-06-04 21:33:39 -0400

    updates

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -46,7 +46,8 @@ export default function Login() {
             <h1>Sign in to your account</h1>
             {
                 error?.message &&
-                    <h3 className="login-error">{error.message}</h3>
+                    <h3 className="login-error">
+                    {error.message}</h3>
             }
 
             <form onSubmit={handleSubmit} className="login-form">
